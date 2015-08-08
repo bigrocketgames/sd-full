a8bb4b61ee5ccb37ab14edc5110e9f92a57ffd44
Nathan Robbert <admin@bigrocketgames.com>
2015-08-07 20:56:46 -0700

    Have the game mode choice menu working.

---
+++ Assets/Scripts/MoveGameModeChoicesLeft.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveGameModeChoicesLeft : MonoBehaviour {
+
+	public RectTransform myRect;
+	
+	private string moveToSpot;
+	private GameObject rightArrow;
+	
+	void Start ()
+	{
+		rightArrow = GameObject.FindGameObjectWithTag("arrowRight");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(myRect.position.x == -12f)
+		{
+			rightArrow.SetActive(false);
+		}
+		else
+		{
+			rightArrow.SetActive(true);
+		}
+		
+		if (moveToSpot == "Left1")
+		{
+			myRect.position = new Vector3(Mathf.MoveTowards(myRect.position.x, -6f, 6*Time.deltaTime),0,0);
+			if(myRect.position.x == -6f)
+			{
+				moveToSpot = "";
+			}
+		}
+		else if (moveToSpot == "Left2")
+		{
+			myRect.position = new Vector3(Mathf.MoveTowards(myRect.position.x, 0f, 6*Time.deltaTime),0,0);
+			if(myRect.position.x == 0f)
+			{
+				moveToSpot = "";
+			}
+		}
+	}
+	
+	void OnMouseUp()
+	{
+		print (myRect.position.x);
+		
+		if (myRect.position.x == -12f)
+		{
+			moveToSpot = "Left1";
+		}
+		else if (myRect.position.x == -6f)
+		{
+			moveToSpot = "Left2";
+		}
+	}
+}
---
+++ Assets/Scripts/MoveGameModeChoicesRight.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveGameModeChoicesRight : MonoBehaviour {
+
+	public RectTransform myRect;
+	
+	private GameObject leftArrow;
+	private string moveToSpot;
+	
+	void Start ()
+	{
+		leftArrow = GameObject.FindGameObjectWithTag("arrowLeft");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(myRect.position.x == 0)
+		{
+			leftArrow.SetActive(false);
+		}
+		else
+		{
+			leftArrow.SetActive(true);
+		}
+			
+		if (moveToSpot == "Right1")
+		{
+			myRect.position = new Vector3(Mathf.MoveTowards(myRect.position.x, -6f, 6*Time.deltaTime),0,0);
+			if(myRect.position.x == -6f)
+			{
+				moveToSpot = "";
+			}
+		}
+		else if (moveToSpot == "Right2")
+		{
+			myRect.position = new Vector3(Mathf.MoveTowards(myRect.position.x, -12f, 6*Time.deltaTime),0,0);
+			if(myRect.position.x == -12f)
+			{
+				moveToSpot = "";
+			}
+		}
+	}
+	
+	void OnMouseUp()
+	{
+		print (myRect.position.x);
+		
+		if (myRect.position.x == 0)
+		{
+			moveToSpot = "Right1";
+		}
+		else if (myRect.position.x == -6f)
+		{
+			moveToSpot = "Right2";
+		}
+	}
+}
